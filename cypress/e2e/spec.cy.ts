58b19f9e90430061b5663fceb42adccbd71717f3
Frank McGuire <fpmcguire@gmail.com>
2022-06-21 18:31:11 +0200

    cleanup

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,6 @@
 describe('App Title Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('ng-dashboard')
+    cy.contains('ng-dashboard-template')
   })
 })
